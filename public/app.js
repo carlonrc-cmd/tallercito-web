8a7af61d476002b8a8e45b9951947de120a16f3e
christian.carlon <christian.carlon@konfio.mx>
2025-12-29 02:41:46 -0600

    Agregado código inicial del blog y estructura del optimizador

---
+++ public/app.js
@@ -0,0 +1,4 @@
+console.log("El Laboratorio de Tallercito.mx está activo 🚀");
+document.querySelector('button').addEventListener('click', () => {
+    alert("¡Próximamente! Estamos configurando el motor de optimización.");
+});
